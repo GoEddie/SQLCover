02e35ce185ec7f671f89c66663f9f4069733084b
Ed Elliott <goeddieuk@gmail.com>
2016-05-05 21:45:28 +0100

    Fix issue with azure bringing covered lines back multiple times and breaking HitCount

--- src/SQLCover/SQLCover/Trace/AzureTraceController.cs
+++ src/SQLCover/SQLCover/Trace/AzureTraceController.cs
@@ -12,7 +12,7 @@ namespace SQLCover.Trace
 ADD EVENT sqlserver.sp_statement_starting(action (sqlserver.plan_handle, sqlserver.tsql_stack) where ([sqlserver].[database_id]=({1})))
 add target package0.ring_buffer(set max_memory = 500
 ) /* not file {2}*/
-WITH (EVENT_RETENTION_MODE=NO_EVENT_LOSS,MAX_DISPATCH_LATENCY=1 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=NONE,TRACK_CAUSALITY=OFF,STARTUP_STATE=OFF) 
+WITH (EVENT_RETENTION_MODE=ALLOW_MULTIPLE_EVENT_LOSS,MAX_DISPATCH_LATENCY=1 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=NONE,TRACK_CAUSALITY=OFF,STARTUP_STATE=OFF) 
 ";
 
         private const string StartTraceFormat = @"alter event session [{0}] on database state = start
@@ -36,9 +36,9 @@ WITH (EVENT_RETENTION_MODE=NO_EVENT_LOSS,MAX_DISPATCH_LATENCY=1 SECONDS,MAX_EVEN
             select x.p.query('.') v from  @x.nodes('/RingBufferTarget/event') as x(p)
 	)
 
-select v.value('(/event/@timestamp)[1]', 'datetime'), v from a
+select  v.value('(/event/@timestamp)[1]', 'datetime'), v from a
 
-    where v.value('(/event/@timestamp)[1]', 'datetime') >= '{1}'";
+    where v.value('(/event/@timestamp)[1]', 'datetime') > '{1}'";
 
         private readonly List<string> _events = new List<string>();
 
@@ -72,8 +72,8 @@ select v.value('(/event/@timestamp)[1]', 'datetime'), v from a
             {
                 try
                 {
-                    var records = Gateway.GetRecords(string.Format(ReadTraceFormat, Name, maxDateReceived));
-
+                    var records = Gateway.GetRecords(string.Format(ReadTraceFormat, Name, maxDateReceived.ToString("yyyy-MM-dd HH:mm:ss.ms")));
+                    Console.WriteLine("errm prob will be 3 of these");
                     foreach (DataRow row in records.Rows)
                     {
                         _events.Add(row.ItemArray[1].ToString());
