06f8aac018a15a17d8d331009ad0e68861d3a1dc
Ed Elliott <goeddieuk@gmail.com>
2018-12-14 16:51:02 +0000

    sniff sqlpackage path

--- src/SQLCover/test/deployLocal.ps1
+++ src/SQLCover/test/deployLocal.ps1
@@ -1,4 +1,26 @@
 
 
+Function Get-SqlPackagePath{
 
-&"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\140\sqlpackage.exe" /Action:Publish /SourceFile:"$($PSCommandPath | Split-Path -Parent)\..\DatabaseProject\bin\Debug\DatabaseProject.dacpac" /TargetServerName:"(localdb)\SQLCover" /TargetDatabaseName:DatabaseProject
+    #WHY? I could have a nuget package that downloads sqlpackage but I know it is on every dev machine and the build agents so just sniff it
+
+    if(Test-Path "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\140\sqlpackage.exe"){
+        "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\140\sqlpackage.exe"
+        return
+    }
+
+    if(Test-Path "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\140\sqlpackage.exe"){
+        "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\140\sqlpackage.exe"
+        return
+    }
+
+
+    if(Test-Path "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\140\sqlpackage.exe"){
+        "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\140\sqlpackage.exe"
+        return
+    }
+
+}
+
+
+&"$(Get-SqlPackagePath)" /Action:Publish /SourceFile:"$($PSCommandPath | Split-Path -Parent)\..\DatabaseProject\bin\Debug\DatabaseProject.dacpac" /TargetServerName:"(localdb)\SQLCover" /TargetDatabaseName:DatabaseProject
