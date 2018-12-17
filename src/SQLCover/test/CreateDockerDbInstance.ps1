4764fadc4f97832fadd2b7b8097b00f33b5f5a7f
Ed Elliott <goeddieuk@gmail.com>
2018-12-17 07:22:40 +0000

    first test using docker instead of localdb

---
+++ src/SQLCover/test/CreateDockerDbInstance.ps1
@@ -0,0 +1,2 @@
+docker run -d -p 1433:1433 -e sa_password=Psgsgsfsfs!!!!! -e ACCEPT_EULA=Y --name=SQLCover microsoft/mssql-server-windows-developer:2017-GA
+
--- src/SQLCover/test/SQLCover.IntegrationTests/ConnectionStringReader.cs
+++ src/SQLCover/test/SQLCover.IntegrationTests/ConnectionStringReader.cs
@@ -21,10 +21,29 @@ namespace SQLCover.IntegrationTests
 
         }
 
-   
+        private static string GetContainerIP()
+        {
+
+            var processInfo = new System.Diagnostics.ProcessStartInfo();
+            processInfo.Arguments = "-Command \"&{ (docker inspect SQLCover | ConvertFrom-Json).NetworkSettings.IPAddress}\"";
+
+            processInfo.FileName = "powershell.exe";
+            processInfo.RedirectStandardOutput = true;
+            processInfo.UseShellExecute = false;
+
+            var process = System.Diagnostics.Process.Start(processInfo);
+            process.WaitForExit();
+            return process.StandardOutput.ReadToEnd().Replace("\r", "").Replace("\n", "");
+
+        }
+        
+
+
+
         public static string GetIntegration()
         {
-            var connectionString = $"Server=np:{GetPipeName()};integrated security=sspi;initial catalog=DatabaseProject";
+            // var connectionString = $"Server=np:{GetPipeName()};integrated security=sspi;initial catalog=DatabaseProject";
+            var connectionString = $"Server=tcp:{GetContainerIP()};uid=sa;pwd=Psgsgsfsfs!!!!!;initial catalog=DatabaseProject";
             Console.WriteLine("Connection String = " + connectionString);
             return connectionString;
         }
