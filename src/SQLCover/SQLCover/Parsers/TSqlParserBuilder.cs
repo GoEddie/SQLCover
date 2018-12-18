9fc5f80e69ff2f6504027b700809732fe41949f5
Ed Elliott <goeddieuk@gmail.com>
2018-12-18 17:38:09 +0000

    dacfx from nuget and allowing up to sql 2019

--- src/SQLCover/SQLCover/Parsers/TSqlParserBuilder.cs
+++ src/SQLCover/SQLCover/Parsers/TSqlParserBuilder.cs
@@ -25,6 +25,12 @@ namespace SQLCover.Parsers
                 case SqlServerVersion.Sql130:
                     return new TSql130Parser(quoted);
 
+                case SqlServerVersion.Sql140:
+                    return new TSql130Parser(quoted);
+
+                case SqlServerVersion.Sql150:
+                    return new TSql130Parser(quoted);
+
                 default:
                     throw new ArgumentOutOfRangeException(nameof(version), version, null);
             }
--- src/SQLCover/test/CreateDockerDbInstance.ps1
+++ src/SQLCover/test/CreateDockerDbInstance.ps1
@@ -1,2 +1,2 @@
-docker run -d -p 1433:1433 -e sa_password=Psgsgsfsfs!!!!! -e ACCEPT_EULA=Y --name=SQLCover microsoft/mssql-server-windows-developer:2017-GA
+docker run -d -p 1433:1433 -e sa_password=Psgsgsfsfs!!!!! -e ACCEPT_EULA=Y --name=SQLCover --memory=10g microsoft/mssql-server-windows-developer:2017-GA
 docker inspect SQLCover
