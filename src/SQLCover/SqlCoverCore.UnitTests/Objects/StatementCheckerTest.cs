dbb17453d510bf5b92a3c4e09f741b82086398f1
lastlink <lastlink@users.noreply.github.com>
2020-10-31 09:14:21 -0400

    dotnet core finished, SQLCover worked in linux, ready to publish as dotnet tool (#61)
    * 07302020 - Added UnitTest Core version
    
    * init action
    
    * more actions experiments
    
    * cross platform build
    
    * test sql publish
    
    * fix yml
    
    * update yml
    
    * update snyk
    
    * cleanup
    
    * publish DatabaseWithTests
    
    * fix domains
    
    * add tsql
    
    * include tsql framework
    
    * some progress
    
    * test github build
    
    * try to fix path
    
    * build solution
    
    * ms build
    
    * more fixes
    
    * rm
    
    * build
    
    * build sql project
    
    * restore
    
    * fix validation
    
    * release build
    
    * use sql script file
    
    * run sql file
    
    * fix new line
    
    * db
    
    * tsqlt setup
    
    * fix unit test
    
    * separate out tsqlt and run unit tests
    
    * try windows test
    
    * more tweaks
    
    * no more mstest
    
    * try junit report
    
    * junit report
    
    * report parser not working
    
    * cleanup
    
    * make pull request
    
    * include push
    
    * code cleanup reformat
    
    * init command line
    
    * validate params
    
    * code coverage
    
    * split output command
    
    * maybe done?
    
    * is it done?
    
    * working result
    
    * more fixes
    
    * dotnet core sql cover
    
    * tryout pipeline
    
    * env var
    
    * rename jobs
    
    * another env var change
    
    * fix needs
    
    * another try
    
    * format env var
    
    * verbose
    
    * fix verbose
    
    * more db madness
    
    * more env var
    
    * more env fun
    
    * double strings...
    
    * env var 3
    
    * generate reports
    
    * more password difficulty
    
    * hard code password
    
    * some fixes
    
    * base assembly name for tool
    
    * fix again
    
    * get report working
    
    * run again
    
    * try again2
    
    * final cleanup
    
    * remove verbose
    
    * add self to contributor
    
    * use a shared library
    
    * test sqlcover
    
    * rm push
    
    Co-authored-by: Andrea Giunta <angiunta@microsoft.com>

---
+++ src/SQLCover/SqlCoverCore.UnitTests/Objects/StatementCheckerTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SQLCover.Objects;
+
+namespace SQLCover.UnitTests.Objects
+{
+    [TestFixture]
+    public class StatementCheckerTests
+    {
+        [Test]
+        public void Statement_Is_Covered_If_CoveredStatement_Has_No_OffsetEnd_And_Statement_Starts_After_CoveredStatement()
+        {
+            var statement = new Statement("", 100, 10, true);
+
+            var coveredStatement = new CoveredStatement
+            {
+                Offset = 105,
+                OffsetEnd = -1,
+                ObjectId = 999
+            };
+
+            var checker = new StatementChecker();
+            Assert.IsTrue(checker.Overlaps(statement, coveredStatement));
+        }
+
+        [Test]
+        public void Statement_Is_Covered_If_Statement_Is_Starts_And_Stops_Within_CoveredStatement()
+        {
+            var statement = new Statement("", 100, 5, true);
+
+            var coveredStatement = new CoveredStatement
+            {
+                Offset = 200,
+                OffsetEnd = 212,
+                ObjectId = 999
+            };
+
+            var checker = new StatementChecker();
+            Assert.IsTrue(checker.Overlaps(statement, coveredStatement));
+        }
+
+        [Test]
+        public void Statement_Is_Not_Covered_If_Statement_Starts_Before_Covered_Statement()
+        {
+
+            var statement = new Statement("", 10, 500, true);
+
+            var coveredStatement = new CoveredStatement
+            {
+                Offset = 200,
+                OffsetEnd = 212,
+                ObjectId = 999
+            };
+
+            var checker = new StatementChecker();
+            Assert.IsFalse(checker.Overlaps(statement, coveredStatement));
+
+
+        }
+
+
+        [Test]
+        public void Statement_Is_Not_Covered_If_Statement_Starts_After_CoveredStatement_Ends()
+        {
+
+            var statement = new Statement("", 1000, 500, true);
+
+            var coveredStatement = new CoveredStatement
+            {
+                Offset = 200,
+                OffsetEnd = 212,
+                ObjectId = 999
+            };
+
+            var checker = new StatementChecker();
+            Assert.IsFalse(checker.Overlaps(statement, coveredStatement));
+
+
+        }
+    }
+}
---
+++ src/SQLCover/SqlCoverCore/Program.cs
@@ -0,0 +1,271 @@
+﻿using CommandLine;
+using Newtonsoft.Json;
+using SQLCover;
+using System;
+using System.IO;
+
+namespace SQLCover.Core
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
+
+            [Option('c', "command", Required = true, HelpText = "Choose command to run from:Get-CoverTSql, Get-CoverExe, Get-CoverRedgateCITest.")]
+            public string Command { get; set; }
+            [Option('e', "exportCommand", Required = true, HelpText = "Choose command to run from:Export-OpenXml, Start-ReportGenerator, Export-Html.")]
+            public string ExportCommand { get; set; }
+            [Option('b', "debug", Required = false, HelpText = "Prints out more output.")]
+            public bool Debug { get; set; }
+            [Option('p', "requiredParams", Required = false, HelpText = "Get required parameters for a command")]
+            public bool GetRequiredParameters { get; set; }
+            [Option('k', "connectionString", Required = false, HelpText = "Connection String to the sql server")]
+            public string ConnectionString { get; set; }
+            [Option('d', "databaseName", Required = false, HelpText = "Default Database")]
+            public string databaseName { get; set; }
+            [Option('q', "query", Required = false, HelpText = "Sql Query, try tSQLt.runAll")]
+            public string Query { get; set; }
+            [Option('o', "outputPath", Required = false, HelpText = "Output Path")]
+            public string OutputPath { get; set; }
+            [Option('a', "args", Required = false, HelpText = "Arguments for an exe file")]
+            public string Args { get; set; }
+            [Option('t', "exeName", Required = false, HelpText = "executable name")]
+            public string ExeName { get; set; }
+        }
+        private enum CommandType
+        {
+            GetCoverTSql,
+            GetCoverExe,
+            GetCoverRedgateCITest,
+            ExportOpenXml,
+            StartReportGenerator,
+            ExportHtml,
+            Unknown
+        }
+        /// <summary>
+        /// should minic arguments from example\SQLCover.ps1
+        /// run by `dotnet run -- -c Get-CoverTSql -r`
+        /// `dotnet run -- -c Get-CoverTSql -e Export-OpenXml -k "Data Source=localhost;Initial Catalog=master;User ID=sa;Password=yourStrong(!)Password" -d DatabaseWithTests -q "tSQLt.runAll" -p TestCoverageOpenXml`
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+                       if (o.Verbose)
+                       {
+                           Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Current Arguments Serialized::" + JsonConvert.SerializeObject(o));
+                           Console.WriteLine("SqlCoverCore! App is in Verbose mode!");
+                       }
+                       else
+                       {
+                           Console.WriteLine($"Current Arguments: -v {o.Verbose} -c {o.Command}");
+                           Console.WriteLine(":::Running SqlCoverCore:::");
+                       }
+                       var cType = CommandType.Unknown;
+                       var eType = CommandType.Unknown;
+                       string[] requiredParameters = null;
+                       string[] requiredExportParameters = null;
+                       switch (o.Command)
+                       {
+                           case "Get-CoverTSql":
+                               cType = CommandType.GetCoverTSql;
+                               requiredParameters = new string[]{
+                                "connectionString",
+                                "databaseName",
+                                "query"};
+                               break;
+                           case "Get-CoverExe":
+                               cType = CommandType.GetCoverExe;
+                               requiredParameters = new string[]{
+                                "connectionString",
+                                "databaseName",
+                                "exeName",
+                                "args"};
+                               break;
+                           case "Get-CoverRedgateCITest":
+                               cType = CommandType.GetCoverRedgateCITest;
+                               requiredParameters = new string[]{
+                                "outputPath"};
+                               break;
+                           default:
+                               Console.WriteLine("Command:" + o.Command + " is not supported");
+                               break;
+                       }
+
+                       switch (o.ExportCommand)
+                       {
+                           case "Export-OpenXml":
+                               eType = CommandType.ExportOpenXml;
+                               requiredExportParameters = new string[]{
+                                "outputPath"};
+                               break;
+                           case "Start-ReportGenerator":
+                               eType = CommandType.StartReportGenerator;
+                               requiredExportParameters = new string[]{"outputPath",
+                                "reportGeneratorPath"};
+                               break;
+                           case "Export-Html":
+                               eType = CommandType.ExportHtml;
+                               requiredExportParameters = new string[]{
+                                "outputPath"};
+                               break;
+                           default:
+                               Console.WriteLine("ExportCommand:" + o.ExportCommand + " is not supported");
+                               break;
+                       }
+
+                       var validParams = eType != CommandType.Unknown && cType != CommandType.Unknown ? validateRequired(o, requiredParameters) : false;
+                       validParams = validParams ? validateRequired(o, requiredExportParameters) : validParams;
+
+                       if (validParams)
+                       {
+                           if (o.GetRequiredParameters)
+                           {
+                               Console.WriteLine(o.Command + " requiredParameters are:" + string.Join(',', requiredParameters));
+                               Console.WriteLine(o.ExportCommand + " requiredParameters are:" + string.Join(',', requiredExportParameters));
+                           }
+                           else
+                           {
+                               Console.WriteLine(":::Running command" + cType.ToString() + ":::");
+                               CodeCoverage coverage = null;
+                               CoverageResult results = null;
+                               // run command
+                               switch (cType)
+                               {
+                                   case CommandType.GetCoverTSql:
+                                       coverage = new CodeCoverage(o.ConnectionString, o.databaseName, null, true, o.Debug);
+                                       results = coverage.Cover(o.Query);
+                                       break;
+                                   case CommandType.GetCoverExe:
+                                       coverage = new CodeCoverage(o.ConnectionString, o.databaseName, null, true, o.Debug);
+                                       results = coverage.CoverExe(o.ExeName, o.Args);
+                                       break;
+                                   case CommandType.GetCoverRedgateCITest:
+                                       //    coverage = new CodeCoverage(o.ConnectionString, o.databaseName, null, true, o.Debug);
+                                       //    coverage.Start();
+                                       // run Redgate SQLRelease Module
+
+                                       //    results = coverage.Stop();
+                                       Console.WriteLine(cType.ToString() + " is not YET supported");
+                                       break;
+                               }
+                               if (coverage != null && results != null)
+                               {
+                                   Console.WriteLine(":::Running exportCommand" + eType.ToString() + ":::");
+                                   var resultString = "";
+                                   var outputPath = "";
+                                   if (!string.IsNullOrWhiteSpace(o.OutputPath))
+                                   {
+                                       outputPath = o.OutputPath;
+                                       if (outputPath.Substring(outputPath.Length - 1, 1) != Path.DirectorySeparatorChar.ToString())
+                                       {
+                                           outputPath += Path.DirectorySeparatorChar;
+                                       }
+                                   }
+                                   else
+                                   {
+                                       outputPath = "Coverage" + Path.DirectorySeparatorChar;
+                                       if (!Directory.Exists(outputPath))
+                                       {
+                                           Directory.CreateDirectory(outputPath);
+                                       }
+                                   }
+                                   switch (eType)
+                                   {
+                                       case CommandType.ExportOpenXml:
+                                           resultString = results.OpenCoverXml();
+                                           results.SaveResult(outputPath + "Coverage.opencover.xml", resultString);
+                                           results.SaveSourceFiles(outputPath);
+                                           break;
+                                       case CommandType.ExportHtml:
+                                           resultString = results.Html();
+                                           results.SaveResult(outputPath + "Coverage.html", resultString);
+                                           results.SaveSourceFiles(outputPath);
+                                           break;
+                                       // thinking this should be separate from program, called directly from ci/cd
+                                       case CommandType.StartReportGenerator:
+                                           Console.WriteLine(eType.ToString() + " is not YET supported");
+                                           break;
+                                   }
+                               }
+                           }
+                       }
+                   });
+        }
+
+        private static bool validateRequired(Options o, string[] requiredParameters, bool export = false)
+        {
+            var valid = true;
+            var requiredString = export ? " is required for this exportCommand" : " is required for this command";
+            foreach (var param in requiredParameters)
+            {
+                switch (param)
+                {
+                    case "connectionString":
+                        if (string.IsNullOrWhiteSpace(o.ConnectionString))
+                        {
+                            Console.WriteLine("connectionString" + requiredString);
+                            valid = false;
+                        }
+                        break;
+                    case "databaseName":
+                        if (string.IsNullOrWhiteSpace(o.databaseName))
+                        {
+                            Console.WriteLine("databaseName" + requiredString);
+                            valid = false;
+                        }
+                        break;
+                    case "query":
+                        if (string.IsNullOrWhiteSpace(o.Query))
+                        {
+                            Console.WriteLine("query" + requiredString);
+                            valid = false;
+                        }
+                        break;
+                    case "outputPath":
+                        if (!string.IsNullOrWhiteSpace(o.OutputPath))
+                        {
+                            if (!Directory.Exists(o.OutputPath))
+                            {
+                                Console.WriteLine("outputPath:" + o.OutputPath + " is not a valid directory.");
+                                valid = false;
+                            }
+
+                        }
+                        else
+                        {
+                            o.OutputPath = "";
+                        }
+                        break;
+                    case "args":
+                        if (string.IsNullOrWhiteSpace(o.Args))
+                        {
+                            Console.WriteLine("args" + requiredString);
+                            valid = false;
+                        }
+                        break;
+
+                    case "exeName":
+                        if (string.IsNullOrWhiteSpace(o.ExeName))
+                        {
+                            Console.WriteLine("exeName" + requiredString);
+                            valid = false;
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Required check on:" + param + " ignored");
+                        // will always be invalid for commands not validated on a required param
+                        valid = false;
+                        break;
+                }
+            }
+            return valid;
+        }
+    }
+}
